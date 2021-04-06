f0e14ce45025aca71b86116123cb254c68d35d0f
Eduardo Garcia Bulsoni <eduardo.bulsoni@modalgr.com.br>
2021-04-06 14:58:14 -0300

    um pouco de polimorfismo (virtual e override)

---
+++ Avulsas/Polimorfismo-RalfLima/Imposto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _11Polimorfismo
+{
+    class Imposto
+    {
+        public virtual void valeAlimentacao(double salario)
+        {
+            Console.WriteLine("Desconto padrão do vale alimentação R$" +(salario * 0.1));
+        }
+        public void valeTransporte(double salario)
+        {
+            Console.WriteLine("Desconto padrão do vale transporte R$"+(salario*0.06));
+        }
+
+    }
+}
---
+++ Avulsas/Polimorfismo-RalfLima/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Estagiario
+            Imposto objetoE = new Estagiario();
+            objetoE.valeAlimentacao(1000);
+            objetoE.valeTransporte(1000);
+            Console.WriteLine("-----------");
+            // Atendente
+            Imposto objetoA = new Atendente();
+            objetoA.valeAlimentacao(2000);
+            objetoA.valeTransporte(2000);
+            Console.WriteLine("-----------");
+            // Gerente
+            Imposto objetoG = new Gerente();
+            objetoG.valeAlimentacao(5000);
+            objetoG.valeTransporte(5000);
+        }
+    }
+}
