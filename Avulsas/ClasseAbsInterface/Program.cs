be711c5e60681edb45850330b09cb15cc4e1b786
Eduardo Garcia Bulsoni <eduardo.bulsoni@modalgr.com.br>
2021-04-07 18:14:16 -0300

    classe abstrata x interface

---
+++ Avulsas/ClasseAbsInterface/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClasseAbsInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // var pagamento = new Pagamento();
+            // var pagamentoViaCc = new PagamentoViaCartao();
+            var pgto = new PagamentoViaCartao();
+
+        }
+    }
+
+    // public interface IPagamento{
+    //     // void MinhaFuncao(string qualquerCoisa);
+    //     DateTime Vencimento {get; set;}
+    //     decimal Valor {get; set;}
+    //     void Pagar(decimal valor);
+    // }
+
+    public abstract class Pagamento
+    {
+        public DateTime Vencimento { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Valor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public virtual void Pagar(decimal valor)
+        {
+            Console.WriteLine("Pgto base");            
+        }
+    }
+
+    public class PagamentoViaCartao : Pagamento
+    {
+        public override void Pagar(decimal valor)
+        {
+            base.Pagar(250);
+        }
+    }
+
+    public class PagamentoViaBoleto
+    {
+        public void Pagar(decimal valor)
+        {
+        }
+    }
+}
