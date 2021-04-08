c9817cd64c5faa277a3eb4332b3607e3c68c90aa
Eduardo Garcia Bulsoni <eduardo.bulsoni@modalgr.com.br>
2021-04-08 15:37:45 -0300

    chapter 04

--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -14,7 +14,6 @@ namespace OdeToFood
 {
     public class Startup
     {
-        currently at:
         // CH03-04
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
---
+++ book cs8.net3/ch04/WritingFunctions/Program.cs
@@ -0,0 +1,108 @@
+﻿using static System.Console;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table:");
+            for (int row = 1; row <= 12; row++)
+            {
+                WriteLine(
+                 $"{row} x {number} = {row * number}");
+            }
+            WriteLine();
+        }
+
+        static void RunTimesTable()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number between 0 and 255: ");
+
+                isNumber = byte.TryParse(
+                    ReadLine(), out byte number);
+
+                if(isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number!");
+                }
+            }
+            while(isNumber);
+        }
+
+        // static void RunTimesTable()
+        // {
+        //     Write("Enter an amount: ");
+        //     string amountInText = ReadLine();
+        //     Write("Enter a two-letter region code: ");
+        //     string region = ReadLine();
+
+        //     if (decimal.TryParse(amountInText, out decimal amount))
+        //     {
+        //         decimal taxToPay = CalculateTax(amount, region);
+        //         WriteLine($"You must pay {taxToPay} in sales tax.");
+        //     }
+        //     else
+        //     {
+        //         WriteLine("You did not enter a valid amount!");
+        //     }
+        // }
+        static decimal CalculateTax (decimal amount, 
+        string twoLetterRegionCode){
+            decimal rate = 0.0M;
+            switch (twoLetterRegionCode)
+            {
+                case "CH": // Switzerland
+                    rate = 0.08M;
+                    break;
+                case "DK":
+                case "NO":
+                    rate = 0.25M;
+                    break;
+                case "GB":
+                case "FR":
+                    rate = 0.2M;
+                    break;
+                case "HU":
+                    rate = 0.27M;
+                    break;
+                // ...etc
+                default:
+                    rate = 0.06M;
+                    break;
+
+            }
+            return amount * rate;
+        }
+
+        static void RunCalculateTax()
+        {
+            Write("Enter an amount: ");
+            string amountInText = ReadLine();
+            Write("Enter a two-letter region code: ");
+            string region = ReadLine();
+
+            if (decimal.TryParse(amountInText, out decimal amount))
+            {
+                decimal taxToPay = CalculateTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in sales tax.");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid amount!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // RunTimesTable();
+            RunCalculateTax();
+        }
+    }
+}
